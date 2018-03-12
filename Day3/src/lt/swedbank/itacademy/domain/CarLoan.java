50705e14a0bf640398e3c10ad4d5d841641e6df7
andrewmic <37101387+andrewmic@users.noreply.github.com>
2018-03-12 12:47:25 +0200

    Review notes.

--- Day3/src/lt/swedbank/itacademy/domain/CarLoan.java
+++ Day3/src/lt/swedbank/itacademy/domain/CarLoan.java
@@ -13,14 +13,18 @@ public class CarLoan extends VehicleLoan implements Comparable<CarLoan> {
 
     @Override
     public int compareTo(CarLoan o) {
+        //Super is not needed (super.getPrice()). "super" should be used when you want to access parent class method/field,
+        // which you are overriding in child (this) class.
         int compareResult = o.getPrice().compareTo(super.getPrice());
         if (compareResult != 0) return compareResult;
 
+        //Float.compare(o.enginePower, enginePower)
         if (o.enginePower != enginePower) {
             if (o.enginePower > enginePower) return 1;
             else return -1;
         }
 
+        //Again, super is not needed here. Also, you can put this line directly in "return" statement.
         compareResult = super.getInterestRate().compareTo(o.getInterestRate());
         return compareResult;
     }
